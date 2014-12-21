63e5f78adf8e50113b0be95b5852d7eb0b12602a
V8q1ez <V8q1ez@users.noreply.github.com>
2014-12-22 00:13:32 +0200

    One more test for the function typedef

--- tests/test_tokenizer_typedef_function.py
+++ tests/test_tokenizer_typedef_function.py
@@ -28,3 +28,23 @@ class compilerTypedefFunction(unittest.TestCase):
         self.assertEqual( SEMICOLON, actualOutput[9].type)
 
         self.assertEqual( EOL, actualOutput[10].type)
+
+    def test_typedef_simple_function_with_spaces(self):
+        input = []
+        input.append('typedef void ( * FOO_TYPE ) ( void ) ; ')
+
+        actualOutput = self.tkz.parseText(input)
+
+        self.assertEqual( TYPEDEF, actualOutput[0].type)
+        self.assertEqual( VOID, actualOutput[1].type)
+        self.assertEqual( PARENTHESIS_LEFT, actualOutput[2].type)
+        self.assertEqual( STAR, actualOutput[3].type)
+        self.assertEqual( LITERAL, actualOutput[4].type)
+        self.assertEqual( 'FOO_TYPE', actualOutput[4].literalValue)
+        self.assertEqual( PARENTHESIS_RIGHT, actualOutput[5].type)
+        self.assertEqual( PARENTHESIS_LEFT, actualOutput[6].type)
+        self.assertEqual( VOID, actualOutput[7].type)
+        self.assertEqual( PARENTHESIS_RIGHT, actualOutput[8].type)
+        self.assertEqual( SEMICOLON, actualOutput[9].type)
+
+        self.assertEqual( EOL, actualOutput[10].type)
