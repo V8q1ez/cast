adb667355c8445b7efebcd3281b0f9a5e61a21c7
V8q1ez <V8q1ez@users.noreply.github.com>
2014-12-21 23:57:58 +0200

    One more test for the string
    Test checks the correct interpretation of a string containing function
    type definition

--- tests/test_tokenizer_define_strings.py
+++ tests/test_tokenizer_define_strings.py
@@ -296,3 +296,19 @@ class preprocessorDefineStrings(unittest.TestCase):
         self.assertEqual( QUOTE, actualOutput[4].type)
 
         self.assertEqual( EOL, actualOutput[5].type)
+
+    def test_define_str_with_function_type_definition(self):
+        input = []
+        input.append('#define a "typedef void(*FOO_TYPE)(void);"')
+
+        actualOutput = self.tkz.parseText(input)
+
+        self.assertEqual( OBJECT_LIKE_MACRO, actualOutput[0].type)
+        self.assertEqual( LITERAL, actualOutput[1].type)
+        self.assertEqual( 'a', actualOutput[1].literalValue)
+        self.assertEqual( QUOTE, actualOutput[2].type)
+        self.assertEqual( STRING, actualOutput[3].type)
+        self.assertEqual( 'typedef void(*FOO_TYPE)(void);', actualOutput[3].literalValue)
+        self.assertEqual( QUOTE, actualOutput[4].type)
+
+        self.assertEqual( EOL, actualOutput[5].type)
