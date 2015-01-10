eed3da9e37660b74f443164c2bd44eb8b6f50aad
V8q1ez <V8q1ez@users.noreply.github.com>
2015-01-10 02:35:27 +0200

    Refactoring: dependencies are injected
    Injection into the constructor arguments

--- src/castle/enchantress.py
+++ src/castle/enchantress.py
@@ -4,9 +4,9 @@ from src.castle.cinderella import cinderella
 from src.castle.prince import prince
 
 class enchantress():
-    def __init__(self):
-        self._cinderella = cinderella()
-        self._prince = prince()
+    def __init__(self, cinderella, prince ):
+        self._cinderella = cinderella
+        self._prince = prince
 
     def formatText(self, rawText):
         tokens = self._cinderella.parseText(rawText)
