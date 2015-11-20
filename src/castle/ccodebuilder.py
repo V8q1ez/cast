6eac61e13e17308f7e1035f10bd7193cac063c44
V8q1ez <V8q1ez@users.noreply.github.com>
2015-11-21 01:50:44 +0200

    Building Context is introduced

--- src/castle/ccodebuilder.py
+++ src/castle/ccodebuilder.py
@@ -3,30 +3,33 @@ __author__ = 'V8q1ez'
 from src.castle.codingrules import CodingRules
 from src.castle.ccodeparser import Grammar
 
+class CCodeBuildingContext():
+    def __init__(self):
+        self.outputText = []
+        self.currentLine = ''
+        self.isMacrosNameHandled = False
+
 class CCodeBuilder():
     def __init__(self, codingRules):
-        self._outputText = []
         self._codingRules = codingRules
 
-    def buildFormattedText(self, tokenList):
-        currentLine = ''
-        isMacrosNameHandled = False
+    def buildFormattedText(self, tokenList, buildingContext):
         for t in tokenList:
             if t.type == Grammar.OBJECT_LIKE_MACRO:
-                currentLine += '#define '
+                buildingContext.currentLine += '#define '
 
             elif t.type == Grammar.LITERAL:
-                if isMacrosNameHandled:
-                    currentLine += t.literalValue
+                if buildingContext.isMacrosNameHandled:
+                    buildingContext.currentLine += t.literalValue
                 else:
-                    currentLine += self._codingRules.handle_macros_name(t.literalValue)
-                    isMacrosNameHandled = True
+                    buildingContext.currentLine += self._codingRules.handle_macros_name(t.literalValue)
+                    buildingContext.isMacrosNameHandled = True
 
             elif t.type == Grammar.PARENTHESIS_LEFT:
-                currentLine += ' ('
+                buildingContext.currentLine += ' ('
             elif t.type == Grammar.PARENTHESIS_RIGHT:
-                currentLine += ')'
+                buildingContext.currentLine += ')'
 
-        self._outputText.append(currentLine)
+        buildingContext.outputText.append(buildingContext.currentLine)
 
-        return self._outputText
+        return buildingContext.outputText
--- tests/ccodebuilder/test_ccodebuilder_define.py
+++ tests/ccodebuilder/test_ccodebuilder_define.py
@@ -9,47 +9,49 @@ from src.castle.ccodebuilder import CCodeBuilder
 from src.castle.codingrules import CodingRules
 from src.castle.ccodeparser import Grammar
 from src.castle.ccodeparser import CCodeParsingContext
+from src.castle.ccodebuilder import CCodeBuildingContext
 
 class CCodeBuilderDefine(unittest.TestCase):
     def setUp(self):
-       pass
+        self._parser = CCodeParser(Grammar())
+        pass
 
     def test_define_nominal(self):
         builder = CCodeBuilder(CodingRules())
-        parser = CCodeParser(Grammar())
-        context = CCodeParsingContext()
+        pContext = CCodeParsingContext()
+        bContext = CCodeBuildingContext()
 
         inputText = """#define A (5)\n"""
         expectedOutput = """#define A (5)\n"""
 
-        actualOutput = builder.buildFormattedText( parser.parseText(inputText.splitlines(), context) )
+        actualOutput = builder.buildFormattedText( self._parser.parseText(inputText.splitlines(), pContext), bContext )
         self.assertEqual( expectedOutput.splitlines(), actualOutput )
 
 
     @mock.patch.object(CodingRules, 'handle_macros_name')
     def test_define_lower_case_literal(self, coding_rules_mock):
         builder = CCodeBuilder(coding_rules_mock)
-        parser = CCodeParser(Grammar())
-        context = CCodeParsingContext()
+        pContext = CCodeParsingContext()
+        bContext = CCodeBuildingContext()
 
         coding_rules_mock.handle_macros_name.return_value = 'LITERAL'
 
         inputText = """#define literal (5)\n"""
         expectedOutput = """#define LITERAL (5)\n"""
 
-        actualOutput = builder.buildFormattedText( parser.parseText(inputText.splitlines(), context) )
+        actualOutput = builder.buildFormattedText( self._parser.parseText(inputText.splitlines(), pContext), bContext )
         self.assertEqual( expectedOutput.splitlines(), actualOutput )
 
 
     @mock.patch.object(CodingRules, 'handle_macros_name')
     def test_define_lower_case_literal_allowed(self, coding_rules_mock):
         builder = CCodeBuilder(coding_rules_mock)
-        parser = CCodeParser(Grammar())
-        context = CCodeParsingContext()
+        pContext = CCodeParsingContext()
+        bContext = CCodeBuildingContext()
         coding_rules_mock.handle_macros_name.return_value = 'literal'
 
         inputText = """#define literal (5)\n"""
         expectedOutput = """#define literal (5)\n"""
 
-        actualOutput = builder.buildFormattedText( parser.parseText(inputText.splitlines(), context) )
+        actualOutput = builder.buildFormattedText( self._parser.parseText(inputText.splitlines(), pContext), bContext )
         self.assertEqual( expectedOutput.splitlines(), actualOutput )
