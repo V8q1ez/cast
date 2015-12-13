ca60c83bfe234243ab9b65d7422e93662402d4c2
V8q1ez <V8q1ez@users.noreply.github.com>
2015-12-14 01:24:30 +0200

    Attempt to follow PEP 8 in one file
    Just to see how it is difficult/useful

--- src/codebuilders/ccodebuilder.py
+++ src/codebuilders/ccodebuilder.py
@@ -1,52 +1,54 @@
 from src.codebuilders.ccodeblockprocessor import CCodeBlockProcessorContext, CCodeBlockProcessor
+from src.codebuilders.macrobuilder import MacroBuilder, MacroBuilderContext
+from src.codebuilders.typedefbuilder import TypeDefBuilder, TypedefBuilderContext
+from src.castle.ccodeparser import Grammar
+
 
 __author__ = 'V8q1ez'
 
-from src.codebuilders.macrobuilder import MacroBuilder, MacroBuilderContext
-from src.codebuilders.typedefbuilder import TypeDefBuilder, TypedefBuilderContext
-from src.castle.ccodeparser import Grammar, CToken
 
-
-class CCodeBuildingContext():
+class CCodeBuildingContext:
     def __init__(self):
         self.outputText = []
         self.currentLine = ''
         self.codingRules = None
+
 
 class CSyntaxError(RuntimeError):
     def __init__(self, arg):
         self.args = arg
 
 
-class CCodeBuilder():
+class CCodeBuilder:
     def __init__(self):
         pass
 
-    def buildFromTokenList(self, tokenList, buildingContext):
+    @classmethod
+    def build_from_token_list(cls, token_list, output_context):
+        assert isinstance(output_context, CCodeBuildingContext)
         index = 0
         previous_block_type = Grammar.UNKNOWN
-        while index < len(tokenList):
-            ccb_context = CCodeBlockProcessorContext( previous_block_type )
-            index = CCodeBlockProcessor.popBlock(tokenList, index, ccb_context)
+        while index < len(token_list):
+            ccb_context = CCodeBlockProcessorContext(previous_block_type)
+            index = CCodeBlockProcessor.popBlock(token_list, index, ccb_context)
             CCodeBlockProcessor.AnalyseAndCorrectBlock(ccb_context)
 
             if previous_block_type == ccb_context.activeBlockType:
-                buildingContext.currentLine += buildingContext.codingRules.get_space_between_similar_blocks()
+                output_context.currentLine += output_context.codingRules.get_space_between_similar_blocks()
 
             if ccb_context.activeBlockType == Grammar.OBJECT_LIKE_MACRO:
-                localContext = MacroBuilderContext()
-                index = MacroBuilder.build(tokenList, index, buildingContext, localContext)
-                buildingContext.previousBlockType = Grammar.UNKNOWN
+                local_context = MacroBuilderContext()
+                index = MacroBuilder.build(token_list, index, output_context, local_context)
+                output_context.previousBlockType = Grammar.UNKNOWN
 
             elif ccb_context.activeBlockType == Grammar.TYPEDEF:
-                localContext = TypedefBuilderContext()
-                localContext.activeBlock = ccb_context.activeBlock
-                TypeDefBuilder.build(buildingContext, localContext)
+                local_context = TypedefBuilderContext()
+                local_context.activeBlock = ccb_context.activeBlock
+                TypeDefBuilder.build(output_context, local_context)
                 previous_block_type = ccb_context.activeBlockType
 
             index += 1
 
-        buildingContext.outputText.append(buildingContext.currentLine)
+        output_context.outputText.append(output_context.currentLine)
 
-        return buildingContext.outputText
-
+        return output_context.outputText
